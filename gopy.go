e481e57dab4675fa4a44ac6220ef52282074a43a
kittuov <krishna@primenumbers.in>
2017-01-07 09:23:43 +0530

    added website layout

--- gopy.go
+++ gopy.go
@@ -1,1 +1,12 @@
-package gopy
+package main
+
+import "net/http"
+
+var (
+	host = "127.0.0.1:9000"
+	webroot = "webapp/"
+)
+
+func main() {
+	http.ListenAndServe(host, http.FileServer(http.Dir(webroot)))
+}
