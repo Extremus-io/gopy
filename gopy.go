d2b963e33148a1bb55a6e899116b31cc15cbc5e6
kittuov <krishna@primenumbers.in>
2017-01-08 11:20:45 +0530

    setup exec streaming service via ws

--- gopy.go
+++ gopy.go
@@ -4,6 +4,9 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"os/exec"
+	"io"
+	"golang.org/x/net/websocket"
 )
 
 var (
@@ -14,6 +17,24 @@ var (
 )
 
 func main() {
+	http.Handle("/api/ws/", websocket.Handler(apiwsCall))
+	http.HandleFunc("/api/", apiCall)
 	http.Handle("/", http.FileServer(http.Dir(webroot)))
 	http.ListenAndServe(host, nil)
+}
+
+func apiCall(w http.ResponseWriter, r *http.Request) {
+	cmd := exec.Command("python", "-u", "func.py")
+	w.WriteHeader(http.StatusOK)
+	stdout, _ := cmd.StdoutPipe()
+	cmd.Start()
+	io.Copy(w, stdout)
+}
+
+func apiwsCall(w *websocket.Conn) {
+	cmd := exec.Command("python", "-u", "func.py")
+	stdout, _ := cmd.StdoutPipe()
+	cmd.Start()
+	io.Copy(w, stdout)
+	w.Close()
 }
